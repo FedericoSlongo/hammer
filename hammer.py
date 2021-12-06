766439793c59d03ff96a00466a498c2e784bc675
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2021-12-06 19:18:36 +0100

    Update hammer.py

--- hammer.py
+++ hammer.py
@@ -20,6 +20,9 @@ def user_agent():
 	uagent.append("Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
 	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
+	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; it-IT; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; it-IT; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; it-IT; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
 	return(uagent)
 
 
